9ce73ba42c9d061ba89081ae64064733a9f12397
Jake Korobellis <jkorobellis@gmail.com>
2024-03-25 21:16:52 -0400

    Flask API test

---
+++ api.py
@@ -0,0 +1,16 @@
+import json
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+employees = [ { 'id': 1, 'name': 'Ashley' }, { 'id': 2, 'name': 'Kate' }, { 'id': 3, 'name': 'Joe' }]
+
+
+@app.route("/employees", methods=["GET"])
+
+def get_employees():
+ return jsonify(employees)
+
+if __name__ == '__main__':
+    app.run(port=5000)
+    print("App running prot 5000")
